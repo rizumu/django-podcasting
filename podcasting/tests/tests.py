db2c381a8759d3d8498c4ddfe79bbfa33c27901b
Frank Becker <fb@alien8.de>
2012-05-21 23:37:30 +0200

    Added test for max_length of AutoSlug field
        - The max length of the AutoSlug field has to be 50.
        - Even if the title does exist already the -int mechanism must
          not exceed the max 50 char for the slug.

--- podcasting/tests/tests.py
+++ podcasting/tests/tests.py
@@ -12,11 +12,20 @@ class PodcastTests(TestCase):
         self.show.save()
         self.episodes = []
         for i in range(0, 10):
-            episode = milkman.deliver(Episode, show=self.show, title="Episode 1")
+            episode = milkman.deliver(Episode, show=self.show,
+                                      title="Episode 1")
             episode.save()
             self.episodes.append(episode)
         self.episode = milkman.deliver(Episode, show=self.show, title="Episode")
         self.episode.save()
+        long_title = "".join(['x' for i in range(51)])
+        self.long_episode1 = milkman.deliver(Episode, show=self.show,
+                                            title=long_title)
+        self.long_episode1.save()
+
+        self.long_episode2 = milkman.deliver(Episode, show=self.show,
+                                             title=long_title)
+        self.long_episode2.save()
 
         self.enclosure = milkman.deliver(Enclosure, episode=self.episodes[0])
         self.enclosure.save()
@@ -42,8 +51,18 @@ class PodcastTests(TestCase):
                           "/podcasts/snowprayers/")
         self.assertEquals(self.episodes[0].get_absolute_url(),
                           "/podcasts/snowprayers/episode-1/")
-
         for i in range(1, 10):
             self.assertEqual(self.episodes[i].get_absolute_url(),
                              "/podcasts/snowprayers/episode-1-{0}/".format(i+1)
             )
+
+    def test_shortend_slug(self):
+        """Test if slug get's shortened to max_length"""
+        self.assertTrue(len(self.long_episode1.slug) == 50,
+                         "Length of slug not <= 50 char!")
+        self.assertTrue(len(self.long_episode2.slug) == 50,
+                        "Length of slug not <= 50 char!")
+        self.assertTrue(self.long_episode2.slug.endswith("-2"),
+                        "Slug doesn't end with '-2' since same title does exist"
+                        " in other episode already!")
+
