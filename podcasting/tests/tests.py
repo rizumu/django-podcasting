dd95a4d16ff75993d8b59039969935f2f061d8de
Frank Becker <fb@alien8.de>
2012-05-21 21:08:02 +0200

    Changed autoslug test to ensure that there are no -int parts cut off
        - While adding an episode called Django-1.4 I noticed that the slug
          was generated as /path/django/. That's bad since I'd like to produce
          a more general episode just called Django later on.
        - This test extends the former one by generating 10 episodes all
          with the same title. The 1st one is suggested to get the slug
          episode-1. The 2nd and up are expected to get the slug episode-1-1.
          Where the last digit is incremented each time.

--- podcasting/tests/tests.py
+++ podcasting/tests/tests.py
@@ -10,14 +10,29 @@ class PodcastTests(TestCase):
     def setUp(self):
         self.show = milkman.deliver(Show, title="snowprayers")
         self.show.save()
-        self.episode = milkman.deliver(Episode, show=self.show, title="Episode 1")
-        self.episode.save()
-        self.enclosure = milkman.deliver(Enclosure, episode=self.episode)
+        self.episodes = []
+        for i in range(0, 10):
+            episode = milkman.deliver(Episode, show=self.show, title="Episode 1")
+            episode.save()
+            self.episodes.append(episode)
+
+        self.enclosure = milkman.deliver(Enclosure, episode=self.episodes[0])
         self.enclosure.save()
 
     def test_podcast(self):
         self.assertEquals(self.show, self.enclosure.episode.show)
+
+    def test_slug_append_int_if_already_exists(self):
+        """Test that the slug is created properly
+        If the slug already exists a '-incremented int' is appended.
+        """
         self.assertEquals(self.show.get_absolute_url(),
                           "/podcasts/snowprayers/")
-        self.assertEquals(self.episode.get_absolute_url(),
+        self.assertEquals(self.episodes[0].get_absolute_url(),
                           "/podcasts/snowprayers/episode-1/")
+
+        for i in range(1, 10):
+            self.assertEqual(self.episodes[i].get_absolute_url(),
+                             "/podcasts/snowprayers/episode-1-{0}/".format(i+1)
+            )
+
