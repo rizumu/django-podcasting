703b701b0929fc9a56f445c6ac1411f4691febbf
rizumu <tom@rizu.mu>
2013-02-07 14:33:21 +0100

    import urls from new location as of django 1.5

--- podcasting/urls.py
+++ podcasting/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from podcasting.views import ShowListView, ShowDetailView, EpisodeListView, EpisodeDetailView
 
 
