c950055f13bdafc354e9ddecf1e52c685c22c0e3
Thomas Schreiber <tom@rizu.fake>
2014-06-28 18:45:50 +0200

    ensure that only supported mime type feed urls may be accessed

--- podcasting/urls_feeds.py
+++ podcasting/urls_feeds.py
@@ -2,17 +2,21 @@ from django.conf.urls import patterns, url
 from django.views.generic.base import RedirectView
 
 from podcasting.feeds import RssShowFeed, AtomShowFeed
+from podcasting.models import Enclosure
+
+
+MIMES = "|".join([enclosure[0] for enclosure in Enclosure.MIME_CHOICES])
 
 
 urlpatterns = patterns(
     "",
 
     # Episode list feed by show (RSS 2.0 and iTunes)
-    url(r"^(?P<show_slug>[-\w]+)/(?P<mime_type>[-\w]+)/rss/$",
+    url(r"^(?P<show_slug>[-\w]+)/(?P<mime_type>{mimes})/rss/$".format(mimes=MIMES),
         RssShowFeed(), name="podcasts_show_feed_rss"),
 
     # Episode list feed by show (Atom)
-    url(r"^(?P<show_slug>[-\w]+)/(?P<mime_type>[-\w]+)/atom/$",
+    url(r"^(?P<show_slug>[-\w]+)/(?P<mime_type>{mimes})/atom/$".format(mimes=MIMES),
         AtomShowFeed(), name="podcasts_show_feed_atom"),
 
     # Episode list feed by show (Media RSS)
