565f33ec904fc0c7c1767b89edfe273e4a20aab9
Thomas Schreiber <rizumu@users.noreply.github.com>
2016-11-02 13:06:49 +0100

    Fix syntax error in Spinx conf.

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = '2011, Thomas Schreiber'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0.1
+version = '1.0.1'
 # The full version, including alpha/beta/rc tags.
 release = '1.0.1'
 
